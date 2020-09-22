a3194b1dd2221ff96263af02c361846bcaf5b627
Rajat Kumar <kumar.rajatprajapati@gmail.com>
2020-09-22 18:16:29 +0530

    emitting and listening message event

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('createMessage', (message) => {
+        console.log('createEmail', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
