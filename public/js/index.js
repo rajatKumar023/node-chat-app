bd929078e8fe7c3ccb33ae1d3bf8059805a2d2b9
Rajat Kumar <kumar.rajatprajapati@gmail.com>
2020-09-23 19:31:28 +0530

    added scripts for messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,25 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
-    const li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    const formattedTime = moment(message.createdAt).format('h:mm a');
+    const template = jQuery('#message-template').html();
+    const html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime,
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-    const li = jQuery('<li></li>');
-    const a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    const formattedTime = moment(message.createdAt).format('h:mm a');
+    const template = jQuery('#location-message-template').html();
+    const html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime,
+    });
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
