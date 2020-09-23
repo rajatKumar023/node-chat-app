97815976af68009cdd0c341ab75dca26528807c2
Rajat Kumar <kumar.rajatprajapati@gmail.com>
2020-09-23 18:44:25 +0530

    added style to app

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    const messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('');
     });
 });
 
@@ -43,13 +45,18 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your Browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        })
+        }, function () {
+            locationButton.removeAttr('disabled').text('Send location');
+        });
     }, function () {
         alert('Unable to fetch your Location');
+        locationButton.removeAttr('disabled').text('Send location');
     })
 });
 
