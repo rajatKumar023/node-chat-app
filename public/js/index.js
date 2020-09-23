ee9615ce269c8ae6c91d7cb53f0a2bb6fe9ed0bf
Rajat Kumar <kumar.rajatprajapati@gmail.com>
2020-09-23 18:10:31 +0530

    added location message

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,15 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    const li = jQuery('<li></li>');
+    const a = jQuery('<a target="_blank">My Current Location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
@@ -26,3 +35,29 @@ jQuery('#message-form').on('submit', function (e) {
 
     });
 });
+
+const locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your Browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function () {
+        alert('Unable to fetch your Location');
+    })
+});
+
+
+
+
+
+
+
+
+
